3c6dcbf989d9d7b6c58c680556ee79cfb020cb9e
PUPPALA MOHIT KUMAR <mail2moht@gmail.com>
2025-11-27 23:36:20 +0530

    Update MedicalRecordController.java

--- src/main/java/com/example/medicalappointmentsystem/controller/MedicalRecordController.java
+++ src/main/java/com/example/medicalappointmentsystem/controller/MedicalRecordController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/records")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class MedicalRecordController {
 
     private final MedicalRecordRepository recordRepository;
